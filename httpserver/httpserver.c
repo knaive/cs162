3fd3c66a6dfea8377837ad4d938f3d3e50d6e222
knaive <wfg2008913@126.com>
2016-11-09 23:16:20 +0800

    multi-threaded http server

--- httpserver/httpserver.c
+++ httpserver/httpserver.c
@@ -45,14 +45,14 @@ int server_proxy_port;
  *   4) Send a 404 Not Found response.
  */
 void reply_with_file(int fd, char *path) {
-      http_start_response(fd, 200);
-      http_send_header(fd, "Content-Type", http_get_mime_type(path));
-      int src_fd = open(path,O_RDONLY);
-      int file_size = get_file_size(src_fd);
-      dprintf(fd, "Content-Length: %d\r\n", file_size);
-      http_end_headers(fd);
-      http_send_file(fd, src_fd);
-      close(src_fd);
+    http_start_response(fd, 200);
+    http_send_header(fd, "Content-Type", http_get_mime_type(path));
+    int src_fd = open(path,O_RDONLY);
+    int file_size = get_file_size(src_fd);
+    dprintf(fd, "Content-Length: %d\r\n", file_size);
+    http_end_headers(fd);
+    http_send_file(fd, src_fd);
+    close(src_fd);
 }
 
 char* join_string(char *str1, char *str2, size_t *size) {
@@ -178,6 +178,11 @@ void handle_files_request(int fd) {
     }
 }
 
+void* request_handle_thread(void *unused) {
+    while(1){
+        handle_files_request(wq_pop(&work_queue));
+    }
+}
 
 
 /*
@@ -208,142 +213,149 @@ void handle_proxy_request(int fd) {
  */
 void serve_forever(int *socket_number, void (*request_handler)(int)) {
 
-  struct sockaddr_in server_address, client_address;
-  size_t client_address_length = sizeof(client_address);
-  int client_socket_number;
+    struct sockaddr_in server_address, client_address;
+    size_t client_address_length = sizeof(client_address);
+    int client_socket_number;
 
-  *socket_number = socket(PF_INET, SOCK_STREAM, 0);
-  if (*socket_number == -1) {
-    perror("Failed to create a new socket");
-    exit(errno);
-  }
-
-  int socket_option = 1;
-  if (setsockopt(*socket_number, SOL_SOCKET, SO_REUSEADDR, &socket_option,
-        sizeof(socket_option)) == -1) {
-    perror("Failed to set socket options");
-    exit(errno);
-  }
-
-  memset(&server_address, 0, sizeof(server_address));
-  server_address.sin_family = AF_INET;
-  server_address.sin_addr.s_addr = INADDR_ANY;
-  server_address.sin_port = htons(server_port);
-
-  if (bind(*socket_number, (struct sockaddr *) &server_address,
-        sizeof(server_address)) == -1) {
-    perror("Failed to bind on socket");
-    exit(errno);
-  }
-
-  if (listen(*socket_number, 1024) == -1) {
-    perror("Failed to listen on socket");
-    exit(errno);
-  }
-
-  printf("Listening on port %d...\n", server_port);
-
-  while (1) {
-    client_socket_number = accept(*socket_number,
-        (struct sockaddr *) &client_address,
-        (socklen_t *) &client_address_length);
-    if (client_socket_number < 0) {
-      perror("Error accepting socket");
-      continue;
+    *socket_number = socket(PF_INET, SOCK_STREAM, 0);
+    if (*socket_number == -1) {
+        perror("Failed to create a new socket");
+        exit(errno);
     }
 
-    printf("Accepted connection from %s on port %d\n",
-        inet_ntoa(client_address.sin_addr),
-        client_address.sin_port);
+    int socket_option = 1;
+    if (setsockopt(*socket_number, SOL_SOCKET, SO_REUSEADDR, &socket_option,
+                sizeof(socket_option)) == -1) {
+        perror("Failed to set socket options");
+        exit(errno);
+    }
 
-    request_handler(client_socket_number);
-    close(client_socket_number);
-  }
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(server_port);
 
-  shutdown(*socket_number, SHUT_RDWR);
-  close(*socket_number);
+    if (bind(*socket_number, (struct sockaddr *) &server_address,
+                sizeof(server_address)) == -1) {
+        perror("Failed to bind on socket");
+        exit(errno);
+    }
+
+    if (listen(*socket_number, 1024) == -1) {
+        perror("Failed to listen on socket");
+        exit(errno);
+    }
+
+    printf("Listening on port %d...\n", server_port);
+
+    while (1) {
+        client_socket_number = accept(*socket_number,
+                (struct sockaddr *) &client_address,
+                (socklen_t *) &client_address_length);
+        if (client_socket_number < 0) {
+            perror("Error accepting socket");
+            continue;
+        }
+
+        printf("Accepted connection from %s on port %d\n",
+                inet_ntoa(client_address.sin_addr),
+                client_address.sin_port);
+
+        /* request_handler(client_socket_number); */
+        wq_push(&work_queue, client_socket_number);
+        close(client_socket_number);
+    }
+
+    shutdown(*socket_number, SHUT_RDWR);
+    close(*socket_number);
 }
 
 int server_fd;
 void signal_callback_handler(int signum) {
-  printf("Caught signal %d: %s\n", signum, strsignal(signum));
-  printf("Closing socket %d\n", server_fd);
-  if (close(server_fd) < 0) perror("Failed to close server_fd (ignoring)\n");
-  exit(0);
+    printf("Caught signal %d: %s\n", signum, strsignal(signum));
+    printf("Closing socket %d\n", server_fd);
+    if (close(server_fd) < 0) perror("Failed to close server_fd (ignoring)\n");
+    exit(0);
 }
 
 char *USAGE =
-  "Usage: ./httpserver --files www_directory/ --port 8000 [--num-threads 5]\n"
-  "       ./httpserver --proxy inst.eecs.berkeley.edu:80 --port 8000 [--num-threads 5]\n";
+"Usage: ./httpserver --files www_directory/ --port 8000 [--num-threads 5]\n"
+"       ./httpserver --proxy inst.eecs.berkeley.edu:80 --port 8000 [--num-threads 5]\n";
 
 void exit_with_usage() {
-  fprintf(stderr, "%s", USAGE);
-  exit(EXIT_SUCCESS);
+    fprintf(stderr, "%s", USAGE);
+    exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char **argv) {
-  signal(SIGINT, signal_callback_handler);
+    signal(SIGINT, signal_callback_handler);
 
-  /* Default settings */
-  server_port = 8000;
-  void (*request_handler)(int) = NULL;
+    /* Default settings */
+    server_port = 8000;
+    void (*request_handler)(int) = NULL;
 
-  int i;
-  for (i = 1; i < argc; i++) {
-    if (strcmp("--files", argv[i]) == 0) {
-      request_handler = handle_files_request;
-      free(server_files_directory);
-      server_files_directory = argv[++i];
-      if (!server_files_directory) {
-        fprintf(stderr, "Expected argument after --files\n");
-        exit_with_usage();
-      }
-    } else if (strcmp("--proxy", argv[i]) == 0) {
-      request_handler = handle_proxy_request;
+    int i;
+    for (i = 1; i < argc; i++) {
+        if (strcmp("--files", argv[i]) == 0) {
+            request_handler = handle_files_request;
+            free(server_files_directory);
+            server_files_directory = argv[++i];
+            if (!server_files_directory) {
+                fprintf(stderr, "Expected argument after --files\n");
+                exit_with_usage();
+            }
+        } else if (strcmp("--proxy", argv[i]) == 0) {
+            request_handler = handle_proxy_request;
 
-      char *proxy_target = argv[++i];
-      if (!proxy_target) {
-        fprintf(stderr, "Expected argument after --proxy\n");
-        exit_with_usage();
-      }
+            char *proxy_target = argv[++i];
+            if (!proxy_target) {
+                fprintf(stderr, "Expected argument after --proxy\n");
+                exit_with_usage();
+            }
 
-      char *colon_pointer = strchr(proxy_target, ':');
-      if (colon_pointer != NULL) {
-        *colon_pointer = '\0';
-        server_proxy_hostname = proxy_target;
-        server_proxy_port = atoi(colon_pointer + 1);
-      } else {
-        server_proxy_hostname = proxy_target;
-        server_proxy_port = 80;
-      }
-    } else if (strcmp("--port", argv[i]) == 0) {
-      char *server_port_string = argv[++i];
-      if (!server_port_string) {
-        fprintf(stderr, "Expected argument after --port\n");
-        exit_with_usage();
-      }
-      server_port = atoi(server_port_string);
-    } else if (strcmp("--num-threads", argv[i]) == 0) {
-      char *num_threads_str = argv[++i];
-      if (!num_threads_str || (num_threads = atoi(num_threads_str)) < 1) {
-        fprintf(stderr, "Expected positive integer after --num-threads\n");
-        exit_with_usage();
-      }
-    } else if (strcmp("--help", argv[i]) == 0) {
-      exit_with_usage();
-    } else {
-      fprintf(stderr, "Unrecognized option: %s\n", argv[i]);
-      exit_with_usage();
+            char *colon_pointer = strchr(proxy_target, ':');
+            if (colon_pointer != NULL) {
+                *colon_pointer = '\0';
+                server_proxy_hostname = proxy_target;
+                server_proxy_port = atoi(colon_pointer + 1);
+            } else {
+                server_proxy_hostname = proxy_target;
+                server_proxy_port = 80;
+            }
+        } else if (strcmp("--port", argv[i]) == 0) {
+            char *server_port_string = argv[++i];
+            if (!server_port_string) {
+                fprintf(stderr, "Expected argument after --port\n");
+                exit_with_usage();
+            }
+            server_port = atoi(server_port_string);
+        } else if (strcmp("--num-threads", argv[i]) == 0) {
+            char *num_threads_str = argv[++i];
+            if (!num_threads_str || (num_threads = atoi(num_threads_str)) < 1) {
+                fprintf(stderr, "Expected positive integer after --num-threads\n");
+                exit_with_usage();
+            }
+            pthread_t *threads = (pthread_t *) malloc(sizeof(pthread_t)*num_threads);
+            pthread_t *p = threads;
+            wq_init(&work_queue);
+            for(int i=0 ; i<num_threads; i++) {
+                pthread_create(p, NULL, request_handle_thread, NULL);
+            }
+        } else if (strcmp("--help", argv[i]) == 0) {
+            exit_with_usage();
+        } else {
+            fprintf(stderr, "Unrecognized option: %s\n", argv[i]);
+            exit_with_usage();
+        }
     }
-  }
 
-  if (server_files_directory == NULL && server_proxy_hostname == NULL) {
-    fprintf(stderr, "Please specify either \"--files [DIRECTORY]\" or \n"
-                    "                      \"--proxy [HOSTNAME:PORT]\"\n");
-    exit_with_usage();
-  }
+    if (server_files_directory == NULL && server_proxy_hostname == NULL) {
+        fprintf(stderr, "Please specify either \"--files [DIRECTORY]\" or \n"
+                "                      \"--proxy [HOSTNAME:PORT]\"\n");
+        exit_with_usage();
+    }
 
-  serve_forever(&server_fd, request_handler);
+    serve_forever(&server_fd, request_handler);
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
--- httpserver/wq.c
+++ httpserver/wq.c
@@ -1,38 +1,44 @@
 #include <stdlib.h>
+#include <pthread.h>
 #include "wq.h"
 #include "utlist.h"
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 /* Initializes a work queue WQ. */
 void wq_init(wq_t *wq) {
 
-  /* TODO: Make me thread-safe! */
-
-  wq->size = 0;
-  wq->head = NULL;
+    wq->size = 0;
+    wq->head = NULL;
 }
 
 /* Remove an item from the WQ. This function should block until there
  * is at least one item on the queue. */
 int wq_pop(wq_t *wq) {
 
-  /* TODO: Make me blocking and thread-safe! */
+    pthread_mutex_lock(&mutex);
+    if(!wq->size) pthread_cond_wait(&cond, &mutex);
 
-  wq_item_t *wq_item = wq->head;
-  int client_socket_fd = wq->head->client_socket_fd;
-  wq->size--;
-  DL_DELETE(wq->head, wq->head);
+    wq_item_t *wq_item = wq->head;
+    int client_socket_fd = wq->head->client_socket_fd;
+    wq->size--;
+    DL_DELETE(wq->head, wq->head);
+    pthread_mutex_unlock(&mutex);
 
-  free(wq_item);
-  return client_socket_fd;
+    free(wq_item);
+    return client_socket_fd;
 }
 
 /* Add ITEM to WQ. */
 void wq_push(wq_t *wq, int client_socket_fd) {
 
-  /* TODO: Make me thread-safe! */
+    wq_item_t *wq_item = calloc(1, sizeof(wq_item_t));
+    wq_item->client_socket_fd = client_socket_fd;
 
-  wq_item_t *wq_item = calloc(1, sizeof(wq_item_t));
-  wq_item->client_socket_fd = client_socket_fd;
-  DL_APPEND(wq->head, wq_item);
-  wq->size++;
+    pthread_mutex_lock(&mutex);
+    DL_APPEND(wq->head, wq_item);
+    wq->size++;
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex);
 }
